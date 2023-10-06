f0d45a760132d8af9fe8963918fc80f3afd490e1
gadas <gadas4415@gmail.com>
2023-10-06 17:07:08 +0700

    v0

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+int main(int argc, char const *argv[])
+{
+    char x = 'a';
+    int y = 0;
+    printf("Input lower letter: ");
+    scanf("%c", &y);
+    x = y;
+    printf("%d %c", y, x);
+    for (int i = 0; i < y; i++)
+    {
+        while ()
+        {
+            /* code */
+        }
+        
+    }
+   
+    return 0;
+}
