ec6acbe996ae4cd38fdbb34ad9e93340afaa776f
gadas <gadas4415@gmail.com>
2023-10-07 12:30:40 +0700

    fanal

--- main.c
+++ main.c
@@ -1,21 +1,35 @@
+//Draws a pyramid of symbols of a given size
 #include <stdio.h>
 
 
-int main(int argc, char const *argv[])
+int main()
 {
     char x = 'a';
-    int y = 0;
+    int y = 0, let = 1;
     printf("Input lower letter: ");
     scanf("%c", &y);
     x = y;
-    printf("%d %c", y, x);
-    for (int i = 0; i < y; i++)
-    {
-        while ()
+
+
+    for (int i = 65; i <= y; i++)
+    {   
+        for (int b = 0; b < y - 65 - let / 2; b++)
         {
-            /* code */
+            printf(" ");
         }
-        
+        for (int c = 0, f = let / 2; c < let; c++)
+        {   
+            if (let == 1){
+                f = 0;
+            }
+            if (c > let / 2){
+                f += 2;
+            }
+            printf("%c", i - f + c);
+        }
+        printf("\n");
+        let += 2;
+
     }
    
     return 0;
